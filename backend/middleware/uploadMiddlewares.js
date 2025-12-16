a3d49a325e6e977000aa464809bc68600ba06335
millyjain09 <millyjain88@gmail.com>
2025-12-17 00:09:45 +0530

    Fixed

--- backend/middleware/uploadMiddlewares.js
+++ backend/middleware/uploadMiddlewares.js
@@ -54,7 +54,7 @@ const path = require("path");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     
-    cb(null, path.join(__dirname, '..','..','uploads'));
+    cb(null, path.join(__dirname, '..','uploads'));
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
